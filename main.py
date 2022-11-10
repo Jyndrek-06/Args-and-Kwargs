6622952ae7b57beca32deea2b0e26a8fb03b8950
Andrzej Blachuta <113551896+Jyndrek-06@users.noreply.github.com>
2022-11-10 20:13:05 +0000

    We added Args in our For Loops.

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-def my_sum(*args):
-  print(args)
+# def my_sum(*args):
+#   print(args)
 
-print(my_sum(3,4,5))
+# print(my_sum(3,4,5))
 
-#args are for entering
-# as many arguments as possible
-def a_sum (*args):
-  for num in args:
-    print(num)
+# #args are for entering
+# # as many arguments as possible
+# def a_sum (*args):
+#   for num in args:
+#     print(num)
 
-print(a_sum(1))
-print(a_sum(1,3))
-print(a_sum(1,3,4,5,6))
+# print(a_sum(1))
+# print(a_sum(1,3))
+# print(a_sum(1,3,4,5,6))
+
+list = [3,4,5,6,7,8,9,45,67]
+def sum_numbers(list):
+  sum = 0
+  for num in list: 
+    sum = sum + num
+  return sum
+  
+print(sum_numbers(list))
+
+
+# args
+def a_sum(*args):
+  # total = 0
+  # for num in args:
+  #   total += num
+  # return total
+  return sum (args)
+
+print(a_sum(4,56,77,88,9,45,76))
+  
